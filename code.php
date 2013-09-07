8c7dcc159e46b3865cde642fc2cc422ecda3bc46
Ashish Lal <ashish@seas.0918.apng.wireless-pennnet.upenn.edu>
2013-09-07 04:35:09 -0400

    update code.php

--- code.php
+++ code.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <title>Code Page</title>
-
+<body>
 Hello
 <?php
-$code = $_POST["fname"];
-echo "code";
+$code = $_POST["code"];
+echo "code:";
 echo $uri;
 ?>
 
